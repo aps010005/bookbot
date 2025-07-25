1692c71974773e63144eadb9c90bec7ddf92d967
aps010005 <calvin.chan9507@gmail.com>
2025-07-26 01:23:36 +0800

    Use sys.argv instead of individual book titles.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import sys
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) == 1:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     word_count = count_words(text)
     char_count = count_characters(text)
@@ -13,34 +18,8 @@ def get_book_text(path):
 
 from stats import count_words
 
-def count_characters(text):
-    characters_dict = {}
-    characters_list = list(text.lower())
-    for character in characters_list:
-        if character.isalpha():
-            if character in characters_dict:
-                characters_dict[character] += 1
-            else:
-                characters_dict[character] = 1
-    characters_dict_list = []
-    for item in characters_dict:
-        value = characters_dict[item]
-        characters_dict_list.append({"char": item, "count": value})
-    characters_dict_list.sort(reverse=True, key=sort_on)
-    #print(characters_dict_list[0]["char"])
-    return characters_dict_list
+from stats import count_characters
 
-def sort_on(dict):
-    return dict["count"]
-
-def print_report(path, word, char):
-    print(f"--- Begin report of {path} ---")
-    print(f"{word} words found in the document")
-    print("")
-    for item in char:
-        character = item["char"]
-        count = item["count"]
-        print(f"The {character} character was found {count} times")
-    print("--- End report ---")
+from stats import print_report
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,34 @@ def count_words(text):
     num_words = len(text.split())
     #print(num_words)
     return num_words
+
+def count_characters(text):
+    characters_dict = {}
+    characters_list = list(text.lower())
+    for character in characters_list:
+        if character.isalpha():
+            if character in characters_dict:
+                characters_dict[character] += 1
+            else:
+                characters_dict[character] = 1
+    characters_dict_list = []
+    for item in characters_dict:
+        value = characters_dict[item]
+        characters_dict_list.append({"char": item, "count": value})
+    characters_dict_list.sort(reverse=True, key=sort_on)
+    #print(characters_dict_list[0]["char"])
+    return characters_dict_list
+
+def sort_on(dict):
+    return dict["count"]
+
+def print_report(path, word, char):
+    print(f"--- Begin report of {path} ---")
+    print(f"{word} words found in the document")
+    print("")
+    for item in char:
+        character = item["char"]
+        count = item["count"]
+        print(f"The {character} character was found {count} times")
+        print(f"{character}: {count}")
+    print("--- End report ---")
